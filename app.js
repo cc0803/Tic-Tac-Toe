e76d2553e90550d0e7c8349d73bf6d343315abed
cc0803 <simongeldbach@gmail.com>
2023-04-21 10:38:46 +0200

    Add style and the basic class mockup

---
+++ app.js
@@ -0,0 +1,12 @@
+const Gameboard = (() => {
+    let gameboardArray = [null, null, null, null, null, null, null, null, null];
+    let markers = ["X", "O"];
+})()
+
+const Player = (m) => {
+    const marker = m;
+}
+
+const Gameflow = () => {
+
+}
