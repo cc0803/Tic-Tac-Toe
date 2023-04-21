53291caafe1c734a124e21c948a38a9063a4914a
cc0803 <simongeldbach@gmail.com>
2023-04-21 10:54:17 +0200

    Create function to add X or O to gameUI

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ const Player = (m) => {
     const marker = m;
 }
 
-const Gameflow = () => {
+const Gameflow = (marker) => {
+    const FIELDS = Array.from(document.querySelectorAll(".game-container>button"));
+    const placeMarker = function() {
+        FIELDS.forEach(field => {
+            field.addEventListener("click", function() {
+                field.textContent = marker;
+            })
+        })
+    }
+    return {placeMarker};
+};
 
-}
+let game = Gameflow("X");
+
+game.placeMarker("X");
